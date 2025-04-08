fdda808f20d52ed021775c045ff30765acdd6ecd
prmntr <bistakenx@gmail.com>
2025-04-08 17:15:04 -0400

    feat: mobile support + small style fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-800">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="src\assets\Tutorly.png" className="h-8" alt="syscall logo" />
+          <img src="src/assets/Tutorly.png" className="h-8" alt="syscall logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Tutorly
           </span>
         </a>
         <button
+          onClick={toggleMobileMenu}
           data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
-          aria-expanded="false"
+          aria-expanded={isMobileMenuOpen}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -25,17 +34,22 @@ function Navbar() {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`${
+            isMobileMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
             <li className="flex items-center space-x-2">
-              <img src="src\assets\dashboard-panel.png" alt="Dashboard Icon" className="w-5 h-5" />
+              <img src="src/assets/dashboard-panel.png" alt="Dashboard Icon" className="w-5 h-5" />
               <a
                 href="/dashboard"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -44,7 +58,7 @@ function Navbar() {
               </a>
             </li>
             <li className="flex items-center space-x-2">
-              <img src="src\assets\student.png" alt="Dashboard Icon" className="w-4.3 h-4.5" />
+              <img src="src/assets/student.png" alt="Classmates Icon" className="w-4 h-4" />
               <a
                 href="/students"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -53,7 +67,7 @@ function Navbar() {
               </a>
             </li>
             <li className="flex items-center space-x-2">
-              <img src="src\assets\messages.png" alt="Dashboard Icon" className="w-4 h-4" />
+              <img src="src/assets/messages.png" alt="Messages Icon" className="w-4 h-4" />
               <a
                 href="/messages"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -62,7 +76,7 @@ function Navbar() {
               </a>
             </li>
             <li className="flex items-center space-x-2">
-              <img src="src\assets\user.png" alt="Dashboard Icon" className="w-4 h-4" />
+              <img src="src/assets/user.png" alt="Profile Icon" className="w-4 h-4" />
               <a
                 href="/profile"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -76,4 +90,5 @@ function Navbar() {
     </nav>
   );
 }
+
 export default Navbar;
