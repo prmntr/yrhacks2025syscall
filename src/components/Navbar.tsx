ed66750d439376390c3160de5d22686be751770a
prmntr <bistakenx@gmail.com>
2025-04-08 12:11:34 -0400

    stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ function Navbar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="src\assets\react.svg" className="h-8" alt="syscall logo" />
+          <img src="src\assets\Tutorly.png" className="h-8" alt="syscall logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            SysCall
+            Tutorly
           </span>
         </a>
         <button
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -12,9 +12,10 @@ function Profile() {
           {/* Name and Description */}
           <div className="ml-6">
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">John Doe</h5>
-            <span className="text-sm text-gray-500 dark:text-gray-400">Grade 69</span>
+            <span className="text-md text-gray-500 dark:text-white">Grade 10</span>
+            <p className="text-gray-400 dark:text-gray-400">Bayview Secondary School</p>
           </div>
-            <div className="flex ml-auto space-x-12 px-5">
+          <div className="flex ml-auto space-x-12 px-5">
             <div className="ml-20">
               <span className="text-sm text-gray-500 dark:text-gray-400">Students Tutored</span>
               <h6 className="mb-1 text-center text-3xl font-bold text-gray-900 dark:text-white">
@@ -35,7 +36,7 @@ function Profile() {
             </div>
           </div>
         </div>
-        <div className="flex justify-center  md:mt-6 mb-5 mt-5">
+        <div className="flex justify-center mt-1 mb-5">
           <a
             href="#"
             className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
