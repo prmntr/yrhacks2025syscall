5dccaeafebb49c93a37ec445566c4720d103fbab
Ethan Deng <ethandeng0624@gmail.com>
2025-04-08 16:09:54 -0400

    hhh

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,10 +1,14 @@
 function Students() {
   return (
+    <div className="bg-gray-900 min-h-screen p-6">
+      <h1 className="text-4xl text-white font-bold mb-6">Students</h1>
+    
     <div>
-      <h1>Tutors</h1>
-      <p>List of tutors will be dfsfdsfdsfdfsdsfisplayed here.</p>
-      <p>The </p>
+      //*<h1>Tutors</h1>
+      //*<p>List of tutors will be dfsfdsfdsfdfsdsfisplayed here.</p>
+      //*<p>The </p>
     </div>
+  </div>
   );
 }
 
